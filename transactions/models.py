397a2147b41f81d13e205e8eb5d3675aef69f73a
Franciscomuro <francisco.muro.se@gmail.com>
2022-05-29 22:09:45 -0500

    add sql save account and transaction

--- transactions/models.py
+++ transactions/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+class Account(models.Model):
+    proprietary = models.CharField("email of the proprietary of the account", max_length=35)
 
 class Transaction(models.Model):
-    date_transaction = models.DateField()
-    cuantity = models.CharField("Cuantity of the transaction", max_length=99)
-    type = models.CharField("Type of the transaction", max_length=99)
+    date_transaction = models.CharField("Date of transaction", max_length=6)
+    quantity = models.FloatField("Quantity of the transaction", max_length=99)
+    proprietary = models.CharField("Account email of the proprietary", max_length=35)
